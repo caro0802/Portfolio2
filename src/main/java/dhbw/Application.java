4a368f7bcaa4ee7fb69b98a91ab600c7711b4fb8
D067795 <D067795@WDFN33986481A.emea.global.corp.sap>
2018-03-22 12:39:14 +0100

    Hoffe mal das klappt

--- src/main/java/dhbw/Application.java
+++ src/main/java/dhbw/Application.java
@@ -12,7 +12,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
- * Created by Florian on 03.03.2018.
+ * My name here
  */
 
 @SpringBootApplication
